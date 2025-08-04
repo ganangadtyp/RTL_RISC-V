eecd5213a8ba17058e7157bcc71308cbc3b00a79
Raihan Fadhil Yanuarsyah <raihan@starteknologi.co.id>
2025-08-05 06:58:24 +0700

    HAL for CP R/W; Main for CoProc Init and Adder Test

--- mmi_hal/hal.c
+++ mmi_hal/hal.c
@@ -1,680 +1,167 @@
 // This file contains a few of standard operation of CP and Comm
 
-#include <hal.h>
+#include "hal.h"
 
-void write_mmi_8(uint8_t addr, uint8_t value){
+uint32_t data_00;
+uint32_t data_04;
+uint32_t data_08;
+uint32_t data_0C;
+uint32_t data_10;
+uint32_t data_14;
+uint32_t data_18;
+uint32_t data_1C;
+
+void mmi(uint8_t addr, uint16_t value){
 	switch (addr)
 	{
-	case (addr_COPCRCEN):
-		data_14 = 0x00000000 | value;
-		(*(uint32_t *) (base_address + 0x10)) = (uint32_t)(data_14); 
+	/* ============================ READ ============================ */
+		// 0x0004_0000
+	case (COPMISO):
+		data_00 = *(volatile uint32_t*)(base_address | 0x00);
+		value = (data_00 & 0x000000FF);
+		// (*(uint32_t *) (ram_base_address + 0x10)) = (uint32_t)(data_04); 
+    break;
+    
+    case (COPSTATR):
+		data_00 = *(volatile uint32_t*)(base_address | 0x00);
+		value = ((data_00 & 0x0000FF00) >> 8);
+    break;
+
+    case (COPSTATR2):
+		data_00 = *(volatile uint32_t*)(base_address | 0x00);
+		value = ((data_00 & 0x00FF0000) >> 16);
+    break;
+
+        // 0x0004_0004
+	case (COPCRCSTAT):
+		data_04 = *(volatile uint32_t*)(base_address | 0x04);
+		value = ((data_04 & 0x0000FF00) >> 8);
+		// (*(uint32_t *) (ram_base_address + 0x10)) = (uint32_t)(data_04); 
 	break;
-	case (addr_COPWREN):
-		data_1C = data_1C & 0xFFFFFF00;
-		data_1C = data_1C | value;
-		(*(uint32_t *) (base_address + 0x10)) = (uint32_t)(data_1C); 
+	case (COPCRCO):
+		data_04 = *(volatile uint32_t*)(base_address | 0x04);
+		value = ((data_04 & 0xFFFF0000) >> 16);
+		// (*(uint32_t *) (ram_base_address + 0x10)) = (uint32_t)(data_04); 
 	break;
-	case (addr_COPWR):
+
+		// 0x0004_0008
+	case (COPWRSTAT):
+		data_08 = *(volatile uint32_t*)(base_address | 0x08);
+		value = (data_04 & 0x000000FF);
+		// (*(uint32_t *) (ram_base_address + 0x10)) = (uint32_t)(data_04); 
+	break;
+	case (COPRD):
+		data_08 = *(volatile uint32_t*)(base_address | 0x08);
+		value = ((data_04 & 0x0000FF00) >> 8);
+		// (*(uint32_t *) (ram_base_address + 0x10)) = (uint32_t)(data_04); 
+	break;
+	case (COPRDSTAT):
+		data_08 = *(volatile uint32_t*)(base_address | 0x08);
+		value = ((data_04 & 0x00FF0000) >> 16);
+		// (*(uint32_t *) (ram_base_address + 0x10)) = (uint32_t)(data_04); 
+	break;
+	case (COPRDLN):
+		data_08 = *(volatile uint32_t*)(base_address | 0x08);
+		value = ((data_04 & 0xFF000000) >> 24);
+		// (*(uint32_t *) (ram_base_address + 0x10)) = (uint32_t)(data_04); 
+	break;
+	/* ---------------------------- READ END ---------------------------- */
+
+	/* ============================   WRITE  ============================ */
+		// 0x0004_000C
+	case (COPMOSI):
+		data_0C &= 0xFFFFFF00;
+		data_0C |= (value & 0xFF);
+		(*(uint32_t *) (base_address + 0x0C)) = (uint32_t)(data_0C);
+    break;
+
+    case (COPCOM):
+	    data_0C &= 0xFFFF00FF;
+        data_0C = data_0C | ((value & 0xFF) << 8);
+		(*(uint32_t *) (base_address + 0x0C)) = (uint32_t)(data_0C);
+    break;
+
+    case (COPTH):
+        data_0C &= 0x0000FFFF;      // Masking, retain the unbothered data
+		data_0C |= (value << 16);   // Shifting right 16, based on the allocation
+		(*(uint32_t *) (base_address + 0x0C)) = (uint32_t)(data_0C); 
+	break;
+
+    	// 0x0004_0010
+	case (COPSRC):
+        data_10 &= 0xFFFF0000;                  // Read data, keeping the unbothered data, reseting the old data that wants to be changed
+		data_10 |= value;						// For 16 bit data no need to mask // giving the new value to the allocation
+		(*(uint32_t *) (base_address + 0x18)) = (uint32_t)(data_10);    // sending the data
+	break;
+    
+    case (COPDST):
+		data_10 &= 0x0000FFFF;      // Masking, retain the unbothered data
+		data_10 |= (value << 16);   // Shifting right 16, based on the allocation
+		(*(uint32_t *) (base_address + 0x10)) = (uint32_t)(data_10); 
+	break;
+
+        // 0x0004_0014
+	case (COPCRCEN):
+		data_14 = 0x00000000 | (value & 0x00FF);		// For 8 bit data mask with 0x00FF to only store relevant bit
+		(*(uint32_t *) (base_address + 0x14)) = (uint32_t)(data_14); 
+	break;
+
+		// 0x0004_0018
+	case (COPCRCINIT):
+		data_18 &= 0xFFFF0000;                  // Read data, keeping the unbothered data, reseting the old data that wants to be changed
+		data_18 |= value;						// For 16 bit data no need to mask // giving the new value to the allocation
+		(*(uint32_t *) (base_address + 0x18)) = (uint32_t)(data_18);    // sending the data
+	break;
+	case (COPCRCIN):
+		data_18 &= 0x0000FFFF;      // Masking, retain the unbothered data
+		data_18 |= (value << 16);   // Shifting right 16, based on the allocation
+		(*(uint32_t *) (base_address + 0x18)) = (uint32_t)(data_18); 
+	break;
+
+		// 0x0004_001C
+	case (COPWREN):
+		data_1C &= 0xFFFFFF00;
+		data_1C |= (value & 0xFF);
+		(*(uint32_t *) (base_address + 0x1C)) = (uint32_t)(data_1C); 
+	break;
+	case (COPWR):
 		data_1C = data_1C & 0xFFFF00FF;
-		data_1C = data_1C | (value << 8);
-		(*(uint32_t *) (base_address + 0x10)) = (uint32_t)(data_1C); 
+		data_1C = data_1C | ((value & 0xFF) << 8);
+		(*(uint32_t *) (base_address + 0x1C)) = (uint32_t)(data_1C); 
 	break;
-	case (addr_COPWRLN):
-		data_1C = data_1C & 0xFF00FFFF;
-		data_1C = data_1C | (value << 16);
-		(*(uint32_t *) (base_address + 0x10)) = (uint32_t)(data_1C); 
+	case (COPWRLN):
+		data_1C &= 0xFF00FFFF;
+		data_1C |= ((value & 0xFF) << 16);
+		(*(uint32_t *) (base_address + 0x1C)) = (uint32_t)(data_1C); 
 	break;
-	case (addr_COPRDEN):
-		data_1C = data_1C & 0x00FFFFFF;
-		data_1C = data_1C | (value << 24);
-		(*(uint32_t *) (base_address + 0x10)) = (uint32_t)(data_1C); 
+	case (COPRDEN):
+		data_1C &= 0x00FFFFFF;
+		data_1C |= ((value & 0xFF) << 24);
+		(*(uint32_t *) (base_address + 0x1C)) = (uint32_t)(data_1C); 
 	break;
+	/* ---------------------------- WRITE END ---------------------------- */
 	
 	default:
 		break;
 	}
 }
-
-uint8_t read_mmi_8(uint8_t addr, uint8_t value){
-	switch (addr)
-	{
-	case (addr_COPCRCO2):
-		return ((*mmi_04 >> 24) & 0xFF);
-	break;
-	
-	default:
-		break;
-	}
-}
-
-// void write_1C(){
-// 	(*(uint32_t *) (base_address + 0x1C)) = (uint32_t)((COPRDEN << 24) | (COPWRLN << 16) | (COPWR << 8) | (COPWREN));
-// }
 
 
 // Initialization, give all inputs 0xFF
 void coprocessor_init(){
-	// COPMOSI 			= 0xFF; //DataIn
-	// COPTH 				= 0xFFFF; //AdThIn
-	// COPSRC 				= 0xFFFF; //AdSrcIn
-	// COPDST 				= 0xFFFF; //AdDstIn
-	// // COPTH2 				= 0xFF; //AdThIn
-	// // COPSRC2 			= 0xFF; //AdSrcIn
-	// // COPDST2				= 0xFF; //AdDstIn
-	// COPWR				= 0xFF;
-	// COPWREN				= 0xFF;
-	// COPRDEN				= 0xFF;
-	// COPCRCINIT_1 	    = 0XFF;
-	// COPCRCINIT_2 	    = 0XFF;
-	// COPCRCI_1 		    = 0XFF;
-	// COPCRCI_2 		    = 0XFF;
-	// COPCRCEN			= 0XFF;
-	// COPCOM 				= 0xFF; //CommandIn
+
+	mmi(COPMOSI, 0x00FF);
+    mmi(COPCOM, 0x00FF);
+    mmi(COPTH, 0xFFFF);
+    mmi(COPSRC, 0xFFFF);
+    mmi(COPDST, 0xFFFF);
     
-    // write_0C();
-	// write_10();
-	// write_14();
-	// write_18();
-	// write_1C();
+    mmi(COPCRCEN, 0x00FF);
+	mmi(COPCRCIN, 0xFFFF);
+	mmi(COPCRCINIT, 0xFFFF);
+	mmi(COPWREN, 0x00FF);
+	mmi(COPWR, 0x00FF);
+	mmi(COPWRLN, 0x00FF);
+	mmi(COPRDEN, 0x00FF);
 
 }
-
-//-------------------------------------------------------------------------------------------
-// SHARED MEMORY CONTROLLER
-// copas block data
-void copy_data_block_sequential(unsigned char lower_source_address2, unsigned char lower_source_address, 
-	unsigned char upper_source_address2, unsigned char upper_source_address,
-	unsigned char destination_address2, unsigned char destination_address){
-	COPCOM = 0x84;
-	write_0C();
-	COPDST = (destination_address2 << 8) | (destination_address);
-	write_10();
-	COPTH = (upper_source_address2 << 8) | upper_source_address;
-	write_0C();
-	COPSRC = (lower_source_address2 << 8) | lower_source_address;
-	write_10();
-	// PERHATIAN: Jangan ubah nilai source address pada saat membaca.
-	// COPSTATR 0x04 = flag jika memory sedang read	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x0C); //0x0C = 0x04 OR 0x08
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void copy_data_block_independent(unsigned char lower_source_address2, unsigned char lower_source_address, 
-	unsigned char upper_source_address2, unsigned char upper_source_address,
-	unsigned char destination_address2, unsigned char destination_address){
-	COPCOM = 0x04;
-	write_0C();
-	COPDST = (destination_address2 << 8) | destination_address;
-	write_10();
-	COPTH = (upper_source_address2 << 8) | upper_source_address;
-	write_0C();
-	COPSRC = (lower_source_address2 << 8) | lower_source_address;
-	write_10();
-	// PERHATIAN: Jangan ubah nilai source address pada saat membaca.
-	// COPSTATR 0x08 = flag jika memory sedang read	
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x0C); //0x0C = 0x04 OR 0x08
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void write_data_independent(unsigned char destination_address2, unsigned char destination_address, unsigned copdata){
-	COPCOM = 0x03;
-	write_0C();
-	COPDST = (destination_address2 << 8 ) | destination_address;
-	write_10();
-	COPMOSI = copdata;
-	write_0C();
-	// PERHATIAN: perhatikan flag penuh instruction buufer 
-
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-
-void write_data_sequential(unsigned char destination_address2, unsigned char destination_address, unsigned copdata){
-	COPCOM = 0x83;
-	write_0C();
-	COPDST = (destination_address2 << 8) | destination_address;
-	write_10();
-	COPMOSI = copdata;
-	write_0C();
-
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-	
-unsigned read_data_sequential(unsigned char source_address2, unsigned char source_address){
-	COPCOM = 0x82;
-	write_0C();
-	COPSRC = (source_address2 << 8) + source_address;
-	write_10();
-	read_00();
-	while (COPSTATR & 0x04);
-	COPCOM = 0xFF;
-	write_0C();
-	// read_00();
-	// while (!(COPSTATR & 0x08));
-
-	do {
-    	read_00();
-	} while (!(COPSTATR & 0x08));
-
-	COPCOM = 0x00;
-	write_0C();
-	return COPMISO;
-}
-
-unsigned read_data_independent(unsigned char source_address2, unsigned char source_address){
-	COPCOM = 0x02;
-	write_0C();
-	COPSRC = (source_address2 << 8) | source_address;
-	write_10();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (!(COPSTATR & 0x08));
-	//while (!(COPSTATR & 0x08));
-	
-	COPCOM = 0x00;
-	write_0C();
-	return COPMISO;
-}
-
-void copy_data_sequential(unsigned char source_address2, unsigned char source_address,
-	unsigned char destination_address2, unsigned char destination_address){
-	COPCOM = 0x81;
-	write_0C();
-	COPSRC = (source_address2 << 8) | source_address;
-	write_10();
-	COPDST = (destination_address2 << 8) | destination_address;
-	write_10();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void copy_data_independent(unsigned char source_address2, unsigned char source_address,
-	unsigned char destination_address2, unsigned char destination_address){
-	COPCOM = 0x01;
-	write_0C();
-	COPSRC = (source_address2 << 8) | source_address;
-	write_10();
-	COPDST = (destination_address2 << 8) |destination_address;
-	write_10();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-//---------------------------------------------------------------------------------------
-
-//---------------------------------------------------------------------------------------
-// Coprocessor controller
-
-void HASH_sequential(){
-	COPCOM = 0x85;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	//while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void HASH_independent(){
-	COPCOM = 0x05;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void BC3_encrypt_k_sequential(){
-	COPCOM = 0x86;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write();
-}
-
-void BC3_encrypt_k_independent(){
-	COPCOM = 0x06;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void BC3_encrypt_sequential(){
-	COPCOM = 0x89;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void BC3_encrypt_independent(){
-	COPCOM = 0x09;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void BC3_decrypt_k_independent(){
-	COPCOM = 0x07;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void BC3_decrypt_k_sequential(){
-	COPCOM = 0x87;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void BC3_decrypt_independent(){
-	COPCOM = 0x0A;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void BC3_decrypt_sequential(){
-	COPCOM = 0x8A;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_keygen_sequential(){
-	COPCOM = 0x8B;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_dec_keygen_sequential(){
-	COPCOM = 0x9C;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_keygen_independent(){
-	COPCOM = 0x0B;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_dec_keygen_independent(){
-	COPCOM = 0x1C;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_encrypt_sequential(){
-	COPCOM = 0x9B;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_decrypt_sequential(){
-	COPCOM = 0x9D;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_encrypt_independent(){
-	COPCOM = 0x1B;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void AES_decrypt_independent(){
-	COPCOM = 0x1D;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Addition_sequential(){
-	COPCOM = 0x8C;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Addition_independent(){
-	COPCOM = 0x0C;
-	write_0C();
-	
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Subtraction_sequential(){
-	COPCOM = 0x8D;
-	write_0C();
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Subtraction_independent(){
-	COPCOM = 0x0D;
-	write_0C();
-
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Multiplication_sequential(){
-	COPCOM = 0x8E;
-	write_0C();
-
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Multiplication_independent(){
-	COPCOM = 0x0E;
-	write_0C();
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Division_sequential(){
-	COPCOM = 0x92;
-	write_0C();
-
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-void Division_independent(){
-	COPCOM = 0x12;
-	write_0C();
-
-	do {
-    	read_00();
-	} while (COPSTATR & 0x04);
-	// while (COPSTATR & 0x04);
-	
-	COPCOM = 0xFF;
-	write_0C();
-}
-
-// ---------------------------------------------------------------------------//
-void send_MMI_1C(){
-    (*(uint32_t *) (base_address + 0x1C)) = (uint32_t)((COPRDEN << 24) | (COPWRLN << 16) | (COPWR << 8) | (COPWREN));
-}
-
-// CONTACTLESS COMMUNICATION FUNCTION
-unsigned char rd_contactless(){
-	unsigned char a;
-	a = COPRD;
-	COPRDEN = 0x00;
-	write_1C();
-	COPRDEN = 0xFF;
-	write_1C();
-	return a;
-}
-
-unsigned char drain_fifo(){
-	unsigned char a;
-	while(!(COPRDSTAT & 0X04)){
-		a = COPRD;
-		COPRDEN = 0x00;
-		write_1C();
-		COPRDEN = 0xFF;
-		write_1C();
-	}
-	return a;
-}
-
-void wr_contactless(unsigned char *data_to_sent, unsigned char *data_lenght){
-	COPWR = *data_to_sent;
-	write_1C();
-	COPWRLN = *data_lenght;
-	write_1C();
-	COPWREN = 0x00;	
-	write_1C();
-	COPWREN = 0xFF;
-	write_1C();
-}
-//-------------------------------------------------------------------------------------------
-
-
-// (*(uint32_t *) (base_address + 0x0C)) = (uint32_t)((COPTH << 16)) | (COPCOM << 8) | (COPMOSI);
-// (*(uint32_t *) (base_address + 0x10)) = (uint32_t)((COPDST << 16) | (COPSRC));
-// (*(uint32_t *) (base_address + 0x14)) = (uint32_t)(0x00000000 | (COPCRCEN));
-// (*(uint32_t *) (base_address + 0x18)) = (uint32_t)((COPCRCI_2 << 24) | (COPCRCI_1 << 16) | (COPCRCINIT_2 << 8) | (COPCRCINIT_1));
-// (*(uint32_t *) (base_address + 0x1C)) = (uint32_t)((COPRDEN << 24) | (COPWRLN << 16) | (COPWR << 8) | (COPWREN));
-
-
-// void write_0C(){
-// 	(*(uint32_t *) (base_address + 0x0C)) = (uint32_t)((COPTH << 16) | (COPCOM << 8) | (COPMOSI));
-// }
-
-// void write_10(){
-// 	(*(uint32_t *) (base_address + 0x10)) = (uint32_t)((COPDST << 16) | (COPSRC));
-// }
-
-// void write_14(){
-// 	(*(uint32_t *) (base_address + 0x14)) = (uint32_t)(0x00000000 | (COPCRCEN));
-// }
-
-// void write_18(){
-// 	(*(uint32_t *) (base_address + 0x18)) = (uint32_t)((COPCRCI_2 << 24) | (COPCRCI_1 << 16) | (COPCRCINIT_2 << 8) | (COPCRCINIT_1));
-// }
-
-// void write_1C(){
-// 	(*(uint32_t *) (base_address + 0x1C)) = (uint32_t)((COPRDEN << 24) | (COPWRLN << 16) | (COPWR << 8) | (COPWREN));
-// }
-
-// void read_00(){
-// 	COPSTATR2	= ((*mmi_00 >> 16) & 0xFF); // StatusOut2
-// 	COPSTATR    = ((*mmi_00 >> 8 ) & 0xFF); // StatusOut
-// 	COPMISO     = ( *mmi_00 & 0xFF);    // DataOut 
-// }
-   
-// void read_04(){
-// 	COPCRCO2	= ((*mmi_04 >> 24) & 0xFF);
-// 	COPCRCO1	= ((*mmi_04 >> 16) & 0xFF);
-// 	COPCRCSTAT  = ((*mmi_04 >> 8 ) & 0xFF);
-// }
-
-// void read_08(){
-// 	COPRDLN	    = ((*mmi_08 >> 24) & 0xFF);
-// 	COPRDSTAT	= ((*mmi_08 >> 16) & 0xFF);
-// 	COPRD	    = ((*mmi_08 >> 8 ) & 0xFF);
-// 	COPWRSTAT   = ( *mmi_08 & 0xFF);
-// }
-
-// void read_COPCRCO2(){
-// 	COPCRCO2	= ((*mmi_04 >> 24) & 0xFF);
-// }
-
-// void read_COPCRCO1(){
-// 	COPCRCO1	= ((*mmi_04 >> 16) & 0xFF);
-// }
-
-// void read_COPCRCSTAT(){
-// 	COPCRCSTAT	= ((*mmi_04 >> 8) & 0xFF);
-// }
--- mmi_hal/hal.h
+++ mmi_hal/hal.h
@@ -4,172 +4,47 @@
 #define HAL_H
 
 /************* Address *************/
-#define base_address    0x00040000
+#define base_address   	0x00040000
+#define ram_base_address   	0x00050000
 
-#define addr_COPSTATR2	    0x0
-#define addr_COPSTATR       0x0
-#define addr_COPMISO        0x0
-#define addr_COPCRCO2	    0x07
-#define addr_COPCRCO1	    0x06
-#define addr_COPCRCSTAT     0x05
-//  0x0addr_
-#define addr_COPRDLN	    0x0B
-#define addr_COPRDSTAT	    0x0A
-#define addr_COPRD	        0x09
-#define addr_COPWRSTAT      0x08
-//  0x0addr_
-#define addr_npu            0x0
-#define addr_COPMOSI        0x0
-#define addr_COPCOM         0x0
-//  0xaddr_
-#define addr_COPTH          0x1
-#define addr_COPDST         0x1
-#define addr_COPSRC         0x1
-//  0x1addr_
-#define addr_COPCRCEN       0x14
-#define addr_COPCRCI_2      0x18
-#define addr_COPCRCI_1      0x19
-#define addr_COPCRCINIT_2   0x1A
-#define addr_COPCRCINIT_1   0x1B
-#define addr_COPRDEN        0x1C
-#define addr_COPWRLN        0x1D
-#define addr_COPWR          0x1E
-#define addr_COPWREN        0x1F
+#define COPSTATR2	  	0x02
+#define COPSTATR      	0x01
+#define COPMISO       	0x00
 
-// COP Output
-// Crypt Output
-uint32_t *mmi_00 = (uint32_t *) base_address + 0x00;
-// Comm Output
-uint32_t *mmi_04 = (uint32_t *) base_address + 0x04;
-uint32_t *mmi_08 = (uint32_t *) base_address + 0x08;
+#define COPCRCO	  		0x06
+#define COPCRCSTAT    	0x05
 
-// Cop Input
-// Crypt Input
-uint32_t *mmi_0C = (uint32_t *) base_address + 0x0C;
-uint32_t *mmi_10 = (uint32_t *) base_address + 0x10;
-// Comm Input
-uint32_t *mmi_14 = (uint32_t *) base_address + 0x14;
-uint32_t *mmi_18 = (uint32_t *) base_address + 0x18;
-// uint32_t *mmi_1C = (uint32_t *) base_address + 0x1C;
+#define COPRDLN	      	0x0B
+#define COPRDSTAT	  	0x0A
+#define COPRD	      	0x09
+#define COPWRSTAT     	0x08
 
-uint32_t data_0C;
-uint32_t data_10;
-uint32_t data_14;
-uint32_t data_18;
-uint32_t data_1C;
+#define npu           	0x0
+#define COPMOSI       	0x0C
+#define COPCOM        	0x0D
 
-#define mmi_1C  (*(uint32_t *) (base_address + 0x1C))
+#define COPTH         	0x0E
+#define COPDST        	0x12
+#define COPSRC        	0x10
 
-/************* End of Address *************/
+#define COPCRCEN      	0x14
+#define COPCRCIN      	0x1A
+#define COPCRCINIT    	0x18
+#define COPRDEN       	0x1C
+#define COPWRLN       	0x1D
+#define COPWR         	0x1E
+#define COPWREN       	0x1F
 
-// uint8_t COPSTATR2	= ((*mmi_00 >> 16) & 0xFF); // StatusOut2
-// uint8_t COPSTATR    = ((*mmi_00 >> 8 ) & 0xFF); // StatusOut
-// uint8_t COPMISO     = ( *mmi_00 & 0xFF);    // DataOut    
+extern uint32_t data_0C;
+extern uint32_t data_10;
+extern uint32_t data_14;
+extern uint32_t data_18;
+extern uint32_t data_1C;
 
-// uint8_t COPCRCO2	= ((*mmi_04 >> 24) & 0xFF);
-// uint8_t COPCRCO1	= ((*mmi_04 >> 16) & 0xFF);
-// uint8_t COPCRCSTAT  = ((*mmi_04 >> 8 ) & 0xFF);
+// extern uint16_t CRCSTAT;
+// extern uint16_t CRCO;
 
-// uint8_t COPRDLN	    = ((*mmi_08 >> 24) & 0xFF);
-// uint8_t COPRDSTAT	= ((*mmi_08 >> 16) & 0xFF);
-// uint8_t COPRD	    = ((*mmi_08 >> 8 ) & 0xFF);
-// uint8_t COPWRSTAT   = ( *mmi_08 & 0xFF);
+extern void coprocessor_init();
+extern void mmi(uint8_t addr, uint16_t value);
 
-
-
-uint8_t COPSTATR2	;
-uint8_t COPSTATR    ;
-uint8_t COPMISO     ;
-
-uint8_t COPCRCO2	;
-uint8_t COPCRCO1	;
-uint8_t COPCRCSTAT  ;
-
-uint8_t COPRDLN	    ;
-uint8_t COPRDSTAT	;
-uint8_t COPRD	    ;
-uint8_t COPWRSTAT   ;
-
-// COP Input
-uint8_t COPMOSI;
-uint8_t COPCOM;
-
-uint16_t COPTH;
-uint16_t COPDST;
-uint16_t COPSRC;
-
-uint8_t COPCRCEN;
-uint8_t COPCRCI_2;
-uint8_t COPCRCI_1;
-uint8_t COPCRCINIT_2;
-uint8_t COPCRCINIT_1;
-uint8_t COPRDEN;
-uint8_t COPWRLN;
-uint8_t COPWR;
-uint8_t COPWREN;
-
-// MMI Register
-// (*(uint32_t *) (base_address + 0x0C)) = uint32_t((COPTH << 16) + (COPCOM << 8) + (COPMOSI));
-// (*(uint32_t *) (base_address + 0x10)) = uint32_t((COPDST << 16) + (COPSRC));
-// (*(uint32_t *) (base_address + 0x14)) = uint32_t(0x00000000 + (COPCRCEN));
-// (*(uint32_t *) (base_address + 0x18)) = uint32_t((COPCRCI2 << 24) + (COPCRCI1 << 16) + (COPCRCINIT2 << 8) + (COPCRCINIT1));
-// (*(uint32_t *) (base_address + 0x1C)) = uint32_t((COPRDEN << 24) + (COPWRLN << 16) + (COPWR << 8) + (COPWREN));
-
-void coprocessor_init();
-
-void write_0C();
-void write_10();
-void write_14();
-void write_18();
-void write_1C();
-void read_00();
-void read_04();
-void read_08();
-void read_COPCRCO2();
-void read_COPCRCO1();
-void read_COPCRCSTAT();
-
-unsigned char rd_contactless();
-unsigned char drain_fifo();
-void wr_contactless(unsigned char *data_to_sent, unsigned char *data_lenght);
-void copy_data_block_sequential(unsigned char lower_source_address2, unsigned char lower_source_address, 
-																unsigned char upper_source_address2, unsigned char upper_source_address,
-																unsigned char destination_address2, unsigned char destination_address);
-void copy_data_block_independent(unsigned char lower_source_address2, unsigned char lower_source_address, 
-																 unsigned char upper_source_address2, unsigned char upper_source_address,
-																 unsigned char destination_address2, unsigned char destination_address);
-void write_data_independent(unsigned char destination_address2, unsigned char destination_address, unsigned copdata);
-void write_data_sequential(unsigned char destination_address2, unsigned char destination_address, unsigned copdata);
-unsigned read_data_sequential(unsigned char source_address2, unsigned char source_address);
-unsigned read_data_independent(unsigned char source_address2, unsigned char source_address);
-void copy_data_sequential(unsigned char source_address2, unsigned char source_address,
-													unsigned char destination_address2, unsigned char destination_address);
-void copy_data_independent(unsigned char source_address2, unsigned char source_address,
-													 unsigned char destination_address2, unsigned char destination_address);
-void HASH_sequential();
-void HASH_independent();
-void BC3_encrypt_k_sequential();
-void BC3_encrypt_k_independent();
-void BC3_encrypt_sequential();
-void BC3_encrypt_independent();
-void BC3_decrypt_k_independent();
-void BC3_decrypt_k_sequential();
-void BC3_decrypt_independent();
-void BC3_decrypt_sequential();
-void AES_keygen_sequential();
-void AES_keygen_independent();
-void AES_encrypt_sequential();
-void AES_encrypt_independent();
-void AES_dec_keygen_sequential();
-void AES_dec_keygen_independent();
-void AES_decrypt_sequential();
-void AES_decrypt_independent();
-void Addition_sequential();
-void Addition_independent();
-void Subtraction_sequential();
-void Subtraction_independent();
-void Multiplication_sequential();
-void Multiplication_independent();
-void Division_sequential();
-void Division_independent();
 #endif  // HAL_H
